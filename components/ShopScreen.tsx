90e6dd166528c6daa5960069eeebdc1b58ad186f
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2024-10-09 11:19:57 +0200

    Implementation : Shop Screen

---
+++ components/ShopScreen.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ShopScreen = () => {
+    return (
+        <View>
+            <Text>Here is where you'll add items to your shopping list!</Text>
+        </View>
+    );
+};
+
+export default ShopScreen;

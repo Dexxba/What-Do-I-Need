6a9833b6f4b5c808b40dd2484c539f81130e05d5
Bogdanov Daniel <daniel.bogdanov@ict.csbe.ch>
2024-10-10 15:49:11 +0200

    Implementation : Organization and Structure with Comments.

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,20 @@
+// Importing React library to use JSX and create components
 import React from 'react';
+
+// Importing the 'registerRootComponent' from Expo to register the main component as the root component
 import { registerRootComponent } from 'expo';
+
+// Importing the AppNavigator component from its relative path
 import AppNavigator from '../components/navigation/AppNavigator';
 
-
-
-const App = () =>{
-    return <AppNavigator/>;
+// Main App component definition
+const App = () => {
+    // This component simply renders the AppNavigator, which manages the navigation between screens
+    return <AppNavigator />;
 };
 
+// Exporting the App component as the default export
 export default App;
-registerRootComponent(App)
+
+// Registering the App component as the root component so that it becomes the entry point of the application
+registerRootComponent(App);
